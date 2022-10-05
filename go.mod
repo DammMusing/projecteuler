4b3c0e528d084a5355d58a8fe3c53d945569bb94
DammMusing <github@dammmusing.com>
2022-10-04 20:47:16 -0400

    prime factoring utilties -- we're using modules!

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
-module go
+module github.com/DammMusing/projecteuler
 
 go 1.19
+
+replace github.com/DammMusing/projecteuler/utils => ./utils
+require github.com/DammMusing/projecteuler/utils v0
---
+++ go/utils/primes.go
@@ -0,0 +1,60 @@
+package utils
+
+/**
+ * Send all the prime values that exist below `limit` through the provided channel.
+ */
+func GatherPrimesLessThan(limit uint64, primes chan uint64) {
+	// Initialize the sieve, but handle the first prime as part of initialization.
+	primes <- 2
+	sieve := NewSieve(limit)
+	sieve.Set(2, true)
+	for i := uint64(3); i < limit; i += 2 {
+		sieve.Set(i, true)
+	}
+
+	for i := uint64(3); i < limit; i++ {
+		if sieve.Get(i) {
+			primes <- i // Send the prime along the channel...
+			for multiple := i * 2; multiple <= limit; multiple += i {
+				// ...and clear all the multiples from the sieve
+				sieve.Set(i, true)
+			}
+		}
+	}
+
+	// Emit a sentinel value to indicate completion.
+	primes <- 0
+}
+
+/** A bitmap datatype for representing the Sieve of Eratosthenes. */
+type Sieve struct {
+	bits []byte
+}
+
+func NewSieve(size uint64) *Sieve {
+	array_size := size >> 3
+	if size&0x07 > 0 {
+		array_size++
+	}
+	return &Sieve{make([]byte, array_size)}
+}
+
+func (s *Sieve) Set(index uint64, value bool) {
+	if index >= uint64(len(s.bits))<<3 {
+		return
+	}
+	pos, bit := index>>3, byte(1<<(index&0x07))
+	if value {
+		s.bits[pos] |= bit
+	} else {
+		s.bits[pos] &= ^bit
+	}
+}
+
+func (s *Sieve) Get(index uint64) bool {
+	if index >= uint64(len(s.bits))<<3 {
+		return false
+	}
+	pos, bit := index>>3, byte(1<<(index&0x07))
+	return s.bits[pos]&bit != 0
+}
